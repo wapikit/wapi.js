1d109c45d0f2512e3b375cbdf1843e0073687737
sarthakjdev <jsarthak448@gmail.com>
2024-07-14 14:10:00 +0530

    fix: build
    Signed-off-by: sarthakjdev <jsarthak448@gmail.com>

--- apps/wapi-ai-chatbot/src/utils/gpt.ts
+++ apps/wapi-ai-chatbot/src/utils/gpt.ts
@@ -1,29 +1,29 @@
-import OpenAI from 'openai'
+import { OpenAI } from 'openai'
 
 const openAiApiKey = process.env.OPEN_AI_API_KEY
 
 if (!openAiApiKey) {
-    throw new Error('OPEN_AI_API_KEY not defined!')
+	throw new Error('OPEN_AI_API_KEY not defined!')
 }
 
 const OpenApiClient = new OpenAI({
-    apiKey: openAiApiKey,
-    project: 'proj_viwVq5nzCR5Mj4TnIw4FQoNO',
-    organization: 'org-wwfaGhYXIA7CBUSg6x8DbKYj'
+	apiKey: openAiApiKey,
+	project: 'proj_viwVq5nzCR5Mj4TnIw4FQoNO',
+	organization: 'org-wwfaGhYXIA7CBUSg6x8DbKYj'
 })
 
 export async function askAi(message: string): Promise<string | null> {
-    try {
-        const chatCompletion = await OpenApiClient.chat.completions.create({
-            messages: [{ role: 'user', content: message }],
-            model: 'gpt-3.5-turbo'
-        })
+	try {
+		const chatCompletion = await OpenApiClient.chat.completions.create({
+			messages: [{ role: 'user', content: message }],
+			model: 'gpt-3.5-turbo'
+		})
 
-        console.log(JSON.stringify({ chatCompletion }))
+		console.log(JSON.stringify({ chatCompletion }))
 
-        return chatCompletion.choices[0].message.content
-    } catch (error) {
-        console.log({ error })
-        return null
-    }
+		return chatCompletion.choices[0].message.content
+	} catch (error) {
+		console.log({ error })
+		return null
+	}
 }
