e914c2566eb31ceb27b9569d57b7aa46e5ddd713
sarthakjdev <jsarthak448@gmail.com>
2024-07-14 17:15:35 +0530

    chore: wip
    Signed-off-by: sarthakjdev <jsarthak448@gmail.com>

--- apps/wapi-ai-chatbot/src/utils/cache.ts
+++ apps/wapi-ai-chatbot/src/utils/cache.ts
@@ -1,28 +1,27 @@
-import { AiConversationRoleEnum, ConversationMessageType } from '~/types'
 import { caching } from 'cache-manager'
 
 const cacheStore = caching({
-	store: 'memory'
+    store: 'memory'
 })
 
 export async function cacheData(params: { key: string; data: any; ttl?: number }) {
-	const { key, ttl, data } = params
-	await cacheStore.set(key, data, { ...(ttl ? { ttl: ttl } : {}) })
+    const { key, ttl, data } = params
+    await cacheStore.set(key, data, { ...(ttl ? { ttl: ttl } : {}) })
 }
 
 export async function getCachedData<T>(key: string): Promise<T> {
-	const response = await cacheStore.get(key)
-	console.log(response)
-	return response as T
+    const response = await cacheStore.get(key)
+    console.log(response)
+    return response as T
 }
 
 export function computeCacheKey(params: { id: string; context: string }) {
-	return `${params.id}-${params.context}`
+    return `${params.id}-${params.context}`
 }
 
 export function getConversationContextCacheKey(phoneNumber: string) {
-	return computeCacheKey({
-		id: phoneNumber,
-		context: 'conversation'
-	})
+    return computeCacheKey({
+        id: phoneNumber,
+        context: 'conversation'
+    })
 }
