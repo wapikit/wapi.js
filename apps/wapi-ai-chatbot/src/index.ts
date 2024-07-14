a42d511735807fc4482e86530b2b54058ff3bb02
sarthakjdev <jsarthak448@gmail.com>
2024-07-14 17:15:53 +0530

    chore: wip
    Signed-off-by: sarthakjdev <jsarthak448@gmail.com>

--- apps/wapi-ai-chatbot/src/index.ts
+++ apps/wapi-ai-chatbot/src/index.ts
@@ -4,39 +4,39 @@ import { askAi } from './utils/gpt'
 import { cacheData, computeCacheKey, getCachedData } from './utils/cache'
 
 async function init() {
-    try {
-        whatsappClient.on('Ready', () => {
-            console.log('Client is ready')
-        })
+	try {
+		whatsappClient.on('Ready', () => {
+			console.log('Client is ready')
+		})
 
-        whatsappClient.on('Error', error => {
-            console.error('Error', error.message)
-        })
+		whatsappClient.on('Error', error => {
+			console.error('Error', error.message)
+		})
 
-        whatsappClient.on('TextMessage', async (event: TextMessageEvent) => {
-            const aiResponse = await askAi(event.text.data.text, event.context.from)
-            await event.reply({
-                message: new TextMessage({
-                    text: aiResponse
-                })
-            })
-        })
+		whatsappClient.on('TextMessage', async (event: TextMessageEvent) => {
+			const aiResponse = await askAi(event.text.data.text, event.context.from)
+			await event.reply({
+				message: new TextMessage({
+					text: aiResponse
+				})
+			})
+		})
 
-        whatsappClient.initiate()
-    } catch (error) {
-        console.error(error)
-        // ! TODO: you may prefer to send a notification to your slack channel or email here
-    }
+		whatsappClient.initiate()
+	} catch (error) {
+		console.error(error)
+		// ! TODO: you may prefer to send a notification to your slack channel or email here
+	}
 }
 
 init().catch(error => console.error(error))
 
 process.on('unhandledRejection', error => {
-    console.error('unhandledRejection', error)
-    process.exit(1)
+	console.error('unhandledRejection', error)
+	process.exit(1)
 })
 
 process.on('uncaughtException', error => {
-    console.error('uncaughtException', error)
-    process.exit(1)
+	console.error('uncaughtException', error)
+	process.exit(1)
 })
